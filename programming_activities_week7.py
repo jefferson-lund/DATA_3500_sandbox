ab83f4daaaf6ae1043bdf64c2acf3790d5a086a6
Jefferson-Century <jlund2000@gmail.com>
2025-10-07 13:29:32 -0600

    merging

--- programming_activities_week7.py
+++ programming_activities_week7.py
@@ -9,31 +9,29 @@ with command "<name>'s favorite color is <color>"
 - use the write function to write to the file
 """
 
-# name = input("Enter your name: ")
-# color = input("Enter your favorite color: ")
-# with open("C:/Users/Jefferson/DATA_3500_Repos/Sandbox/Activity_7.1/color_file", "w") as file:
-#     file.write(f"{name}'s favorite color is {color}")
+name = input("Enter your name: ")
+color = input("Enter your favorite color: ")
+with open("C:/Users/Jefferson/DATA_3500_Repos/Sandbox/Activity_7.1/color_file", "w") as file:
+    file.write(f"{name}'s favorite color is {color}")
 
 
 
 """
 Programming Activity 2
-
 Create a NumPy array of 100 numbers, initialized to 0. Then, change the 
 array from 0s to random numbers.
 """
 
-# import numpy
-# import random
+import numpy
+import random
 
-<<<<<<< HEAD
-# array = numpy.zeros(100)
-# print(array)
-# #learned about enumerate and differences between for loops on lists vs arrays in memory
-# for i, value in enumerate(array):
-#     array[i] = random.randint(0,100)
+array = numpy.zeros(100)
+print(array)
+#learned about enumerate and differences between for loops on lists vs arrays in memory
+for i, value in enumerate(array):
+    array[i] = random.randint(0,100)
 
-# print(array)
+print(array)
 
 """
 Write a program, which loads a json file "person.json" into a Python 
@@ -59,7 +57,6 @@ with open("C:/Users/Jefferson/DATA_3500_Repos/Sandbox/person-1.json", "w") as fi
 with open("C:/Users/Jefferson/DATA_3500_Repos/Sandbox/person-1.json", "r") as file:
     person = json.load(file)
     print(f"Contents of json file: {person}")
-=======
->>>>>>> 0b3a5fde3bc109cb9d93372e3926e596f389fd2e
+
 
 
