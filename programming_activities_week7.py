902624281b0d5d9b4d0fdcef03a16b4b77e07bc7
Jefferson-Century <jlund2000@gmail.com>
2025-10-07 13:27:10 -0600

    Finished week 7

--- programming_activities_week7.py
+++ programming_activities_week7.py
@@ -23,12 +23,40 @@ Create a NumPy array of 100 numbers, initialized to 0. Then, change the
 array from 0s to random numbers.
 """
 
-import numpy
-import random
+# import numpy
+# import random
 
-array = numpy.array[100]
-for i in array:
-    array.append(0)
+# array = numpy.zeros(100)
+# print(array)
+# #learned about enumerate and differences between for loops on lists vs arrays in memory
+# for i, value in enumerate(array):
+#     array[i] = random.randint(0,100)
 
-print(array)
+# print(array)
+
+"""
+Write a program, which loads a json file "person.json" into a Python 
+dictionary. Change the contents of person["age"] by adding 1. Save the 
+updated dictionary to person.json, and verify the contents of person.json 
+have been updated.
+- load person.json in to a Python dictionary using the json.load() function
+- update the value of person["age"], increase by 1
+- save the Python dictionary to person.json
+- open person.json and verify the "age" value has increased by 1
+"""
+import json
+
+with open("C:/Users/Jefferson/DATA_3500_Repos/Sandbox/person-1.json", "r") as file:
+    person = json.load(file)
+    person["age"] += 1
+
+with open("C:/Users/Jefferson/DATA_3500_Repos/Sandbox/person-1.json", "w") as file:
+    json.dump(person, file)
+# print(person["age"])
+
+#verify that the age updated
+with open("C:/Users/Jefferson/DATA_3500_Repos/Sandbox/person-1.json", "r") as file:
+    person = json.load(file)
+    print(f"Contents of json file: {person}")
+
 
