5cc7a0e06ef9af94f9f026fe5fb738090414e6b1
Jefferson Lund <97694747+jefferson-lund@users.noreply.github.com>
2025-11-19 16:19:09 +0000

    Fixed main and created function for pulling

--- stock_market_trading/main.py
+++ stock_market_trading/main.py
@@ -1,19 +1,32 @@
-
-import json
 import requests
-import pandas as pd
+import json
 
-# keys
-date_key = "" #I know I need all the dates at some point maybe use iterator?
-key1 = "Time Series (Daily)"
-key2 = "#4. close"
-url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&outputsize=full&apikey=NG9C9EPVYBMQT0C8"
+tickers = ["AAPL", "TSLA", "NVDA"]
+# , "SONY", "COST", "META", "AMZN", "SNAP", "DAL", "FIX"]
 
-request = requests.get(url)
-request_list = request.text
-request_dict = json.loads(request.text)
-print(request_dict)
-# with open("/workspaces/DATA_3500_sandbox/stock_market_trading/AAPL.csv", "w") as f:
+def initial_data_pull(ticker):
+    # keys
+    key1 = "Time Series (Daily)"
+    key2 = "4. close"
+    # date_key = "" # I know I need all the dates (maybe use an iterator variable?  ??)
 
-#     for date in request_dict[key1].keys():
-#         f.write()        
+    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker}&outputsize=full&apikey=NG9C9EPVYBMQT0C8"
+
+    request = requests.get(url)
+    request_dictionary = json.loads(request.text)
+    # print(request_dictiona    r   y)
+
+
+    file = open(f"/workspaces/DATA_3500_sandbox/stock_market_trading/{ticker}.csv", "w")
+
+    lines =  []
+
+    for date in request_dictionary[key1].keys():
+        lines.append(date + ", " + request_dictionary[key1][date][key2] + "\n")
+
+    lines = lines[::-1]
+    file.writelines(lines)
+    file.close()
+
+for ticker in tickers:
+    initial_data_pull(ticker)
